158c6ab443a2ff1401a7d49aaf9e87b53ceeb661
Pacco Tan <tan.pacco@gmail.com>
2025-04-13 23:02:23 -0500

    fixed special token usage

--- tokenize_text.py
+++ tokenize_text.py
@@ -7,13 +7,24 @@ with open('data/train.jsonl') as f:
 with open("data/train.txt","w") as f:
     for d in data:
         f.write(d["prompt"] + " " + d["completion"] + "\n")
-spm.SentencePieceTrainer.train('--input=data/train.txt --model_prefix=data/tokenizer --vocab_size=10000 --model_type=bpe --character_coverage=1')
+spm.SentencePieceTrainer.train('--input=data/train.txt --user_defined_symbols=<bos>,<eos> --model_prefix=data/tokenizer --vocab_size=10000 --model_type=bpe --character_coverage=1 --pad_id=9999 --bos_piece=<bos> --eos_piece=<eos>')
 
 sp = spm.SentencePieceProcessor()
 sp.load('data/tokenizer.model')
-with open('data/train.jsonl') as f:
+with open('data/test.jsonl') as f:
     data = [json.loads(line) for line in f]
+    data = [(sp.encode_as_ids(json_object["prompt"]),sp.encode_as_ids(json_object["completion"])) for json_object in data]
+    lens = [len(x) + len(y) for x, y in data]
+print(data[(lens).index(min(lens))],(lens).index(min(lens)))
 
-ids = sp.encode_as_ids("which do you prefer? dogs or cats?")
-print(ids)
+count = 0
+for x,y in data:
+    if(0 in x or 0 in y):
+        count += 1
+print(count)
+
+for i in range(5):
+    ids = sp.encode("<bos>Which do you prefer? Dogs or cats?")
+    print(ids)
+
 print(sp.decode_ids(ids))
